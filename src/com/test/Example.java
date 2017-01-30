e581b95bd0b90f9a4efe6f4e3eea8218d3c245b9
Oxana Gritsenko <ohryt@softserveinc.com>
2017-01-30 10:38:40 +0100

    Initial commit

---
+++ src/com/test/Example.java
@@ -0,0 +1,31 @@
+package com.test;
+
+public class Example {
+
+    private int i = 11;
+    private static int k = 22;
+    private static Integer l = 33;
+
+    public static void main(String[] args) {
+        System.out.println("My first program");
+        System.out.println("l: " + l);
+        Example main = new Example();
+        System.out.println("i: " + main.i);
+
+        sayHello();
+        main.saySomething();
+
+    }
+
+    private static void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    private void saySomething() {
+        System.out.println("Something");
+    }
+
+    private Example() {
+
+    }
+}
